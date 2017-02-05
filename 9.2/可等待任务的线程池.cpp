28ea54c5bab2dc01116abc76110e7c109d0884eb
Administrator <Administrator@CN--20160628ECP>
2017-02-05 11:44:18 +0800

    快速排列

--- 9.2/可等待任务的线程池.cpp
+++ 9.2/可等待任务的线程池.cpp
@@ -4,6 +4,7 @@
 #include "thread_safe_queue.h"
 #include <vector>
 #include <numeric>
+#include <list>
 
 class function_wrapper
 {
@@ -61,6 +62,15 @@ class thread_pool
 
 		}
 	}
+
+	void run_pending_task()
+	{
+		function_wrapper task;
+		if (work_queue.try_pop(task))
+			task();
+		else
+			std::this_thread::yield();
+	}
 public:
 	template<typename FunctionType>
 	std::function<typename std::result_of<FunctionType()>::type>	//1
@@ -111,3 +121,47 @@ struct accumulate_block
 		return std::accumulate(first, last, T());//2
 	}
 };
+
+template<typename T>
+struct sorter	//1
+{
+	thread_pool pool;	//2
+	std::list<T> do_sort(std::list<T>& chunk_data)
+	{
+		if (chunk_data.empty())
+			return chunk_data;
+		std::list<T> result;
+		result.splice(result.begin(), chunk_data, chunk_data.begin());
+		T const& partition_val = *result.begin();
+
+		typename std::list<T>::iterator divide_point =
+			std::partition(chunk_data.begin(), chunk_data.end(),
+				[&](T const& val) {return val < partition_val};);
+
+		std::list<T> new_lower_chunk;
+		new_lower_chunk.splice(new_lower_chunk.end(),
+			chunk_data, chunk_data.begin(), divide_point);
+
+		std::future<std::list<T>> new_lower =	//3
+			pool.submit(std::bind(&sorter::do_sort, this, std::move(new_lower_chunk)));
+
+		std::list<T> new_higher(do_sort(chunk_data));
+
+		result.splice(result.end(), new_higher);
+		while (!new_lower.wait_for(std::chrono::seconds(0)) ==
+			std::future_status::timeout)
+			pool.run_pending_task();	//4
+
+		result.splice(result.begin(), new_lower.get());
+		return result;
+	}
+};
+
+template<typename T>
+std::list<T> parallel_quick_sort(std::list<T> input)
+{
+	if (input.empty())
+		return input;
+	sorter<T> s;
+	return s.do_sort(input);
+}
